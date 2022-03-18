747a22d3b3bb01f1c1a672633c0905b086b99b87
yousef moien hussain <zeroyousef13@gmail.com>
2022-03-18 15:37:38 +0300

    cw8

--- CW1.playground/Contents.swift
+++ CW1.playground/Contents.swift
@@ -2,4 +2,11 @@ import UIKit
 
 // التمرين الأول
 // ClassWork 1
-
+var name="yousef moien"
+var birthday=2004
+var age=2022-birthday
+print("hello my name is \(name) my age is \(age)")
+var number1 = "8"
+var number2 = 4
+var number3 = 2.5
+var total = Int(number1) ?? 0 + number2
